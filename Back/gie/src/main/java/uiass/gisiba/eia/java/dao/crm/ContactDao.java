3caa90d67e90a23006e979e4ba3139bcd3ca6108
Ayoub <ayou2017boudra@gmail.com>
2024-03-15 10:39:57 +0000

    yfiyf

--- Back/gie/src/main/java/uiass/gisiba/eia/java/dao/crm/ContactDao.java
+++ Back/gie/src/main/java/uiass/gisiba/eia/java/dao/crm/ContactDao.java
@@ -2,11 +2,7 @@ package uiass.gisiba.eia.java.dao.crm;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import javax.persistence.NonUniqueResultException;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
-import org.hibernate.SessionFactory;
 
 import uiass.gisiba.eia.java.entity.crm.Contact;
 
