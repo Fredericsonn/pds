b7dd554b8c461b8879f3fbbe2b9b7e653088a05b
Ayoub <ayou2017boudra@gmail.com>
2024-04-22 20:57:51 +0100

    inventory dao methods defined

--- Back/gie/src/main/java/uiass/gisiba/eia/java/dao/inventory/InventoryItemDao.java
+++ Back/gie/src/main/java/uiass/gisiba/eia/java/dao/inventory/InventoryItemDao.java
@@ -1,9 +1,15 @@
 package uiass.gisiba.eia.java.dao.inventory;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 import uiass.gisiba.eia.java.dao.crm.HibernateUtility;
+import uiass.gisiba.eia.java.entity.inventory.InventoryItem;
+import uiass.gisiba.eia.java.entity.inventory.Product;
 
 public class InventoryItemDao implements iInventoryItemDao {
 
@@ -13,5 +19,41 @@ public class InventoryItemDao implements iInventoryItemDao {
     public InventoryItemDao() {
         this.em= HibernateUtility.getEntityManger();
 	    tr=em.getTransaction();
+    }
+
+    @Override
+    public InventoryItem getInventoryItemById(int itemId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInventoryItemById'");
+    }
+
+    @Override
+    public List<InventoryItem> getAllInventoryItems() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllInventoryItems'");
+    }
+
+    @Override
+    public int getItemQuantity(int itemId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getItemQuantity'");
+    }
+
+    @Override
+    public void addInventoryItem(Product product, int quantity, LocalDate dateAdded) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addInventoryItem'");
+    }
+
+    @Override
+    public void deleteInventoryItem(int itemId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteInventoryItem'");
+    }
+
+    @Override
+    public void updateInventoryItem(int id, Map<String, Object> columnsNewValues) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateInventoryItem'");
     }
 }
--- Back/gie/src/main/java/uiass/gisiba/eia/java/dao/inventory/iInventoryItemDao.java
+++ Back/gie/src/main/java/uiass/gisiba/eia/java/dao/inventory/iInventoryItemDao.java
@@ -1,5 +1,22 @@
 package uiass.gisiba.eia.java.dao.inventory;
 
+import java.time.LocalDate;
+import java.util.*;
+
+import uiass.gisiba.eia.java.entity.inventory.InventoryItem;
+import uiass.gisiba.eia.java.entity.inventory.Product;
+
 public interface iInventoryItemDao {
 
+    InventoryItem getInventoryItemById(int itemId);
+
+    List<InventoryItem> getAllInventoryItems();
+
+    int getItemQuantity(int itemId);
+
+    void addInventoryItem(Product product, int quantity, LocalDate dateAdded);
+
+    void deleteInventoryItem(int itemId);
+
+    void updateInventoryItem(int id, Map<String,Object> columnsNewValues);
 }
